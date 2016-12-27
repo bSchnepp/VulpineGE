ea0845eec3447557fc1ea5112447979cd31c59f7
Brian Schnepp <bschneppdev@gmail.com>
2016-12-27 11:38:18 -0800

    Fixed stuff

--- src/Vulpine_GTK.cpp
+++ src/Vulpine_GTK.cpp
@@ -7,9 +7,11 @@
  NOTICE:
  	 This project requires GTK+, which is licensed under the LGPL.
  	 Sources for that are here: https://github.com/GNOME/gtk
+ 	 For absolute clarity, I'm using libgtk-3-0: 3.22.5-1 (amd64), obtained from Ubuntu repositories.
  	 Since I don't distribute binaries, linking this will be your own problem. (Ideally you want to dynamically link)
  	 License for GTK is here: https://github.com/GNOME/gtk/blob/master/COPYING
  	 Do note that GTK is __only__ used for tools, NOT for the actual game-world stuff itself. (That is done via SDL)
+ 	 LGPL looks scary, please tell me if I'm doing something wrong.
  ============================================================================
  */
 
